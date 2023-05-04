0ad138f8aa2a88c1da6332fbf7523f67636d7570
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2023-05-05 00:28:07 +0530

    Sample JS calculator implemented

---
+++ index.js
@@ -0,0 +1,32 @@
+const outputEl = document.querySelector(".text");
+const ClearEl = document.querySelector(".AC");
+const deleteEl = document.querySelector(".DEL");
+const equalEl = document.querySelector(".equal");
+const valEl = document.querySelectorAll(".val");
+
+//It adds values in outputEL
+console.log(valEl);
+valEl.forEach((val) => {
+  val.addEventListener("click", () => {
+    outputEl.value += val.value;
+  });
+});
+
+// It clears all the digits in the output
+ClearEl.addEventListener("click", () => {
+  outputEl.value = "";
+});
+
+// It clears all the digits in the output
+deleteEl.addEventListener("click", () => {
+  outputEl.value = outputEl.value.slice(0, -1);
+});
+
+// If there is no error, it evaluates the output value or else it shows syntax error as a alert message.
+equalEl.addEventListener("click", () => {
+  try {
+    outputEl.value = eval(outputEl.value);
+  } catch (e) {
+    alert(e);
+  }
+});
